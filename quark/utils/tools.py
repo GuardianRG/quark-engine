b1de48e8cec7c73a01b291be6393c82f96e9c421
krnick <sungboss2004@gmail.com>
2020-06-03 23:10:00 +0800

    refactor the variable name

--- quark/utils/tools.py
+++ quark/utils/tools.py
@@ -8,29 +8,28 @@ def remove_dup_list(element):
     return list(set(element))
 
 
-def contains(small_list, big_list):
+def contains(subset_to_check, target_list):
     """
-    check the sequence pattern within two list.
+    Check the sequence pattern within two list.
     -----------------------------------------------------------------
-    small = ["getCellLocation", "sendTextMessage"]
-    big_list = ["put", "getCellLocation", "query", "sendTextMessage"]
+    subset_to_check = ["getCellLocation", "sendTextMessage"]
+    target_list = ["put", "getCellLocation", "query", "sendTextMessage"]
     then it will return true.
     -----------------------------------------------------------------
-    small = ["getCellLocation", "sendTextMessage"]
-    big_list = ["sendTextMessage", "put", "getCellLocation", "query"]
+    subset_to_check = ["getCellLocation", "sendTextMessage"]
+    target_list = ["sendTextMessage", "put", "getCellLocation", "query"]
     then it will return False.
-
     """
-    big_copy = copy.deepcopy(big_list)
+    target_copy = copy.deepcopy(target_list)
 
-    # Delete elements that do not exist in the small list
-    for item in big_copy:
-        if item not in small_list:
-            big_copy.remove(item)
+    # Delete elements that do not exist in the subset_to_check list
+    for item in target_copy:
+        if item not in subset_to_check:
+            target_copy.remove(item)
 
-    for i in range(len(big_copy) - len(small_list) + 1):
-        for j in range(len(small_list)):
-            if big_copy[i + j] != small_list[j]:
+    for i in range(len(target_copy) - len(subset_to_check) + 1):
+        for j in range(len(subset_to_check)):
+            if target_copy[i + j] != subset_to_check[j]:
                 break
         else:
             return True
